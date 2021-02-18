ae0f6522e27bf31710a303330ad7e64d61a0db2c
Luis Angelo Belmonte <lafbelmonte@gmail.com>
2021-02-18 08:56:48 +0800

    websockets

--- client.js
+++ client.js
@@ -1,23 +1,23 @@
-// const WebSocket = require('ws');
+const WebSocket = require('ws');
 
-// const ws = new WebSocket('ws://localhost:80');
+const ws = new WebSocket('ws://localhost:80');
 
-// ws.on('open', function open() {
-//   ws.send('something');
-// });
-
-// ws.on('message', function incoming(data) {
-//   console.log(data);
-// });
-
-// ws.on('error', function(error) {
-//     console.log(error)
-// })
-
-
-const ioClient = require('socket.io-client')
-const client = ioClient.connect(`ws://localhost:80`, {transports: ['websocket']});
-
-client.on('outgoing', (data) => {
-  console.log(data)
+ws.on('open', function open() {
+  ws.send('something');
 });
+
+ws.on('message', function incoming(data) {
+  console.log(data);
+});
+
+ws.on('error', function(error) {
+    console.log(error)
+})
+
+
+// const ioClient = require('socket.io-client')
+// const client = ioClient.connect(`ws://nodejs-odk7.ginto.io`, {transports: ['websocket']});
+
+// client.on('outgoing', (data) => {
+//   console.log(data)
+// });
--- server.js
+++ server.js
@@ -1,37 +1,28 @@
-// const WebSocket = require('ws');
+const WebSocket = require('ws');
 
-// const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 80 });
 
-// wss.on('connection', function connection(ws) {
-//   ws.on('message', function incoming(message) {
-//     console.log('received: %s', message);
-//   });
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
 
-//   setInterval(function intervalFunc() {
-//     ws.send('something')
-//   }, 3000);
-
-
-// });
-
-// wss.on('error', function connection(ws) {
-//   ws.on('message', function incoming(message) {
-//     console.log('received: %s', message);
-//   });
-
-//   ws.send('something');
-// });
-
-const http = require('http')
-
-const nodeServer = http.createServer();
-const server = require('socket.io')(nodeServer);
-
-nodeServer.listen(80);
-
-server.sockets.on('connection', (socket) => {
-    setInterval(function intervalFunc() {
-    socket.emit('outgoing', 'Hello over websockets');
+  setInterval(function intervalFunc() {
+    ws.send('something')
   }, 3000);
+
 });
+
+// const http = require('http')
+
+// const nodeServer = http.createServer();
+// const server = require('socket.io')(nodeServer);
+
+// nodeServer.listen(80);
+
+// server.sockets.on('connection', (socket) => {
+//     setInterval(function intervalFunc() {
+//     socket.emit('outgoing', 'Hello over websockets');
+//   }, 3000);
+// });
 
