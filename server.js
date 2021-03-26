4b5b239ab9dd007bb62f2791684b315d06781d08
Luis Angelo Belmonte <lafbelmonte@gmail.com>
2021-03-26 11:23:52 +0800

    update sockets

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-const WebSocket = require('ws');
+// const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 80 });
+// const wss = new WebSocket.Server({ port: 80 });
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
+// wss.on('connection', function connection(ws) {
+//   ws.on('message', function incoming(message) {
+//     console.log('received: %s', message);
+//   });
 
-  setInterval(function intervalFunc() {
-    ws.send('something')
-  }, 3000);
-
-});
-
-// const http = require('http')
-
-// const nodeServer = http.createServer();
-// const server = require('socket.io')(nodeServer);
-
-// nodeServer.listen(80);
-
-// server.sockets.on('connection', (socket) => {
-//     setInterval(function intervalFunc() {
-//     socket.emit('outgoing', 'Hello over websockets');
+//   setInterval(function intervalFunc() {
+//     ws.send('something')
 //   }, 3000);
+
 // });
+
+const http = require('http')
+
+const nodeServer = http.createServer();
+const server = require('socket.io')(nodeServer);
+
+nodeServer.listen(80);
+
+server.sockets.on('connection', (socket) => {
+    setInterval(function intervalFunc() {
+    socket.emit('outgoing', 'Hello over websockets');
+  }, 3000);
+});
 
